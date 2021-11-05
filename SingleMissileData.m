df4806a4f48009c5a9aed8c7cce7370b3b3d23ed
Divyajyoti02 <67417654+Divyajyoti02@users.noreply.github.com>
2021-11-05 13:01:26 +0530

    Fix SingleMissileData.m

--- SingleMissileData.m
+++ SingleMissileData.m
@@ -37,19 +37,35 @@ function pMat = SingleMissileData(v,theta,phi,x0,y0,z0,t0,ts,rangexy,rangez,tmax
     uppT = t0 + 2*v*sin(pi*theta/180)/g;
 
     syms x(t) y(t) z(t);
-    x(t) = piecewise(t > lowT && t < uppT, v * (t - t0) * cos(theta) * cos(phi) + x0, NaN);
-    y(t) = piecewise(t > lowT && t < uppT, v * (t - t0) * cos(theta) * sin(phi) + y0, NaN);
-    z(t) = piecewise(t > lowT && t < uppT, v * (t - t0) * sin(theta) - g * ((t - t0)^2) / 2, NaN);
+    x(t) = piecewise(t > lowT & t < uppT, v * (t - t0) * cos(theta) * cos(phi) + x0, NaN);
+    y(t) = piecewise(t > lowT & t < uppT, v * (t - t0) * cos(theta) * sin(phi) + y0, NaN);
+    z(t) = piecewise(t > lowT & t < uppT, v * (t - t0) * sin(theta) - g * ((t - t0)^2) / 2 + z0, NaN);
 
     pT = T;
     pX = x(T);
     pY = y(T);
     pZ = z(T);
-    pMat = [pT pX pY pZ];
+    pMatSym = [pT; pX; pY; pZ];
 
-    for i = 1:size(pMat, 1)
-        if (pMat(i, 2)^2 + pMat(i, 3)^2 > rangexy^2) || (pMat(i, 4) > rangez && pMat(i, 4) < 0)
-            pMat(i, 2:4) = [NaN NaN NaN];
+    for i = 1:size(pMatSym, 1)
+        if (pMatSym(i, 2)^2 + pMatSym(i, 3)^2 > rangexy^2) || (pMatSym(i, 4) > rangez && pMatSym(i, 4) < 0)
+            pMatSym(i, 2:4) = [NaN NaN NaN];
         end
     end
+
+    pMat = (double(pMatSym))';
+
+    grid on;
+    set(gca,'xminorgrid','on','yminorgrid','on', 'zminorgrid', 'on');
+    hold on
+
+    n = tmax / ts + 1;
+    h = plot3(pMat(1, 2), pMat(1, 3), pMat(1, 4));
+
+    for k = 2:n
+        pause(ts)
+        set(h, 'xdata', (pMat(1:k, 2))', 'ydata', (pMat(1:k, 3))', 'zdata', (pMat(1:k, 4))')
+    end
+
+    hold off
 end
